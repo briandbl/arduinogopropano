cda2d95505640b562ce974f6f2bde8d48bb098c9
Konrad Iturbe <github@chernowii.com>
2014-01-31 11:31:47 +0100

    Create tl-arduinogopropano.ino

---
+++ tl-arduinogopropano.ino
@@ -0,0 +1,72 @@
+/*
+ArduinoGoProPano Timelapse file.
+MAKE SURE YOUR GOPRO HAS AUTOEXEC:ASH IN THE ROOT LEVEL OF THE SD!!!
+*/
+
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+ 
+
+{
+  for(pos = 0; pos < 45; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+{
+  delay(5000)
+}
+{
+for(pos = 45; pos < 90; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+{
+  delay(5000)
+}
+{
+for(pos = 90; pos < 135; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+{
+  delay(5000)
+}
+{
+for(pos = 135; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+{
+  delay(5000)
+}
+{
+for(pos = 180; pos < 225; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+{
+  delay(5000)
+}
+{
+for(pos = 225; pos < 270; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+}
